429e71e36f9aaa37a53ffb0a026ef7b4bbb06f19
drworm75 <dwgames@gmail.com>
2017-04-09 14:59:54 -0500

    Added images.  D3 working.  A winner is you

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,20 @@
-alert('JSReady');
+function d3() {
+ 	var number = Math.floor((Math.random() * 3) + 1);
+ 	return number
+
+}
+
+$(document).on('click', '.reveal', function(){
+	console.log($(".cross").hasClass());
+	$(".cross:not([class*='hidden'])").addClass('hidden');
+	var firstNum = d3();
+	var secondNum = d3();
+	while (secondNum === firstNum) {
+		secondNum = d3();
+	}
+	$(".fighter:nth-of-type(" + firstNum + ")").find('.hidden').removeClass('hidden');
+	$(".fighter:nth-of-type(" + secondNum + ")").find('.hidden').removeClass('hidden');
+	$(".winner").html($(".hidden").data('winner'));
+	$(this).replaceWith(`<button class="reveal">Fight Again?</button>`);
+
+})
